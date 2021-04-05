a3e9e3c394827836d1ba720ce231b6217384b4a5
danielj32 <danieljuanito21@hotmail.com>
2021-04-04 20:24:41 -0500

    add new tasks

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -7,31 +7,26 @@ determines if a given data set represents a valid UTF-8 encoding.
 def validUTF8(data):
     """
     Return: True if data is a valid UTF-8 encoding, else return False
-    A character in UTF-8 can be 1 to 4 bytes long
-    The data set can contain multiple characters
-    The data will be represented by a list of integers
-    Each integer represents 1 byte of data, therefore you only need
-    to handle the 8 least significant bits of each integer
     """
-    ctByt = 0
+    ctBytes = 0
     first = 1 << 7
     second = 1 << 6
 
-    for j in data:
-        mask_of_byt = 1 << 7
-        if ctByt == 0:
-            while mask_of_byt & j:
-                ctByt += 1
-                mask_of_byt = mask_of_byt >> 1
+    for i in data:
+        mask_n_byte = 1 << 7
+        if ctBytes == 0:
+            while mask_n_byte & i:
+                ctBytes += 1
+                mask_n_byte = mask_n_byte >> 1
 
-            if ctByt == 0:
+            if ctBytes == 0:
                 continue
-            if ctByt == 1 or ctByt > 4:
+            if ctBytes == 1 or ctBytes > 4:
                 return False
         else:
-            if not(j & first and not (j & second)):
+            if not (i & first and not (i & second)):
                 return False
-        ctByt -= 1
-        if ctByt == 0:
-            return True
-        return False
+        ctBytes -= 1
+    if ctBytes == 0:
+        return True
+    return False
