f7017891ed4303d009f14ade1315bfa2af95851d
danielj32 <danieljuanito21@hotmail.com>
2021-04-08 01:26:17 -0500

    fix add

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
+"""unlock boxes """
+
+
 def canUnlockAll(boxes):
     """
     method that determines if all the boxes can be opened
     * boxes is a list of lists
     * A key with the same number as a box opens that box
     """
-    firstbox = [0]
-    for k in firstbox:
-        for keyMaster in boxes[k]:
-            if keyMaster not in firstbox:
-                if keyMaster < len(boxes):
-                    firstbox.append(keyMaster)
-    if len(firstbox) == len(boxes):
-        return True
-    return False
+    for key in range(1, len(boxes) - 1):
+        validation = False
+        for idx in range(len(boxes)):
+            validation = key in boxes[idx] and key != idx
+            if validation:
+                break
+        if not validation:
+            return validation
+    return True
