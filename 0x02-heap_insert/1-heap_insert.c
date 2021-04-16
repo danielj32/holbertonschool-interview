2dff1887012956d272e9c75857b739d4b5ae216d
danielj32 <danieljuanito21@hotmail.com>
2021-04-16 12:58:15 -0500

    fix add add new functions

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,23 +1,69 @@
 #include "binary_trees.h"
 /**
- * heap_insert - creates a binary tree node
- * @root: double pointer
- * @value: value to store
- *
- * Return: The address of the inserted node, or NULL on failure
- */
-
+* heap_insert - insert node in heap max
+* @root: pointer double to root heap
+* @value: value n node to insert
+* Return: pointer to node insert or NULL if err
+*/
 heap_t *heap_insert(heap_t **root, int value)
 {
+	size_t newNode;
+	heap_t *firstNode;
 
 	if (*root == NULL)
+		return (*root = binary_tree_node(NULL, value));
+	newNode = get_sizeOf(*root) + 1;
+	firstNode = wthIndex(*root, value, 1, newNode);
+
+	while (firstNode && firstNode->parent && firstNode->n > firstNode->parent->n)
 	{
-		*root = binary_tree_node(*root, value);
-		return (*root);
+		firstNode->n = firstNode->parent->n;
+		firstNode->parent->n = value;
+		firstNode = firstNode->parent;
 	}
-	else
-	{
+	return (firstNode);
+}
+/**
+ * wthIndex - Inserts node level ordered
+ * @root: Type binary_tree pointer of the parent node
+ * @value: Type int value of value inserted into node
+ * @idx: Type int index of the node
+ * @newNode: number the nodes + 1
+ * Return: Node inserted
+ */
+heap_t *wthIndex(binary_tree_t *root, int value, size_t idx, size_t newNode)
+{
+	heap_t *nodel, *noder;
+	size_t parent = newNode / 2;
+
+	if (root == NULL)
 		return (NULL);
+	if (idx == parent)
+	{
+		if (newNode % 2)
+			return (root->right = binary_tree_node(root, value));
+		else
+			return (root->left = binary_tree_node(root, value));
 	}
-	return (NULL);
+	nodel = wthIndex(root->left, value, 2 * idx, newNode);
+	noder = wthIndex(root->right, value, 2 * idx + 1, newNode);
+	if (nodel)
+		return (nodel);
+	else
+		return (noder);
+}
+/**
+ * get_sizeOf - gets number of nodes in heap
+ * @root: pointer to root node
+ * Return: number of nodes
+ */
+size_t get_sizeOf(heap_t *root)
+{
+	int left = 0, right = 0;
+
+	if (root == NULL)
+		return (0);
+	left = get_sizeOf(root->left);
+	right = get_sizeOf(root->right);
+	return (1 + left + right);
 }
--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -4,6 +4,8 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -24,4 +26,6 @@ typedef struct binary_tree_s heap_t;
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 heap_t *heap_insert(heap_t **root, int value);
+heap_t *wthIndex(binary_tree_t *root, int value, size_t idx, size_t nNode);
+size_t get_sizeOf(heap_t *root);
 #endif
