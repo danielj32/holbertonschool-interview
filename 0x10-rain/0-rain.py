03b20f2105c2279549e14b1762508ff4f0548174
danielj32 <danieljuanito21@hotmail.com>
2021-05-27 17:10:00 -0500

    add task

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+rain function
+"""
+
+
+def rain(walls):
+    """Given a list of non-negative integers
+    representing walls of width 1,
+    calculate how much water
+    will be retained after it rains.
+    """
+    if not walls:
+        return 0
+    if len(walls) < 3:
+        return 0
+    waterRainbow = 0
+    for key in range(1, len(walls) - 1):
+        l_column = max(walls[:key])
+        r_column = max(walls[key + 1:])
+        minium = min(l_column, r_column)
+        if walls[key] < minium:
+            waterRainbow += minium - walls[key]
+    return waterRainbow
