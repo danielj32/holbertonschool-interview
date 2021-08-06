25b98ca2945220c8ebfcf0f59a9cdede2a94c0e7
danielj32 <danieljuanito21@hotmail.com>
2021-08-05 20:04:46 -0500

    add task

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" 0. Change comes
+from within """
+
+
+def makeChange(coins, total):
+    """ Given a pile of coins
+    of different values,
+    determine the fewest number
+    of coins needed to meet
+    a given amount total """
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    sm = 0
+    aux = 0
+    temp = 0
+    n_coins = len(coins)
+    while sm < total and aux < n_coins:
+        while coins[aux] <= total - sm:
+            sm += coins[aux]
+            temp = temp + 1
+            if sm is total:
+                return temp
+        aux = aux + 1
+    return -1
