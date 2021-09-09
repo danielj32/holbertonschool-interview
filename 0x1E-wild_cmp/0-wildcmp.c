74dcea2f7b1854384894431e298afd5fa4981f9d
danielj32 <danieljuanito21@hotmail.com>
2021-09-09 18:47:24 -0500

    function that compares two strings

---
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * wildcmp - function that compares two strings
+ * @s1: first string
+ * @s2: can contain the special character *
+ * Return: 1 if the strings can be considered identical, otherwise 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (s1 == NULL)
+		return (0);
+	if (s2 == NULL)
+		return (0);
+	if (!s1[0] && !s2[0])
+		return (1);
+	if (s2[0] == '*')
+	{
+		if (!s1[0] && s2[1])
+			return (0);
+		else if (wildcmp(s1, s2 + 1))
+			return (1);
+		else
+			return (wildcmp(s1 + 1, s2));
+	}
+	if (s1[0] == s2[0])
+		return (wildcmp(s1 + 1, s2 + 1));
+	else
+		return (0);
+}
---
+++ 0x1E-wild_cmp/holberton.h
@@ -0,0 +1,5 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+int wildcmp(char *s1, char *s2);
+#endif
